e3c12287331eb524f8847b1a50ff24ff4c14dc13
V. Hill <67915924+v-hill@users.noreply.github.com>
2021-07-12 11:33:46 +0100

    Bug fixes to database merger

--- utilities/merge_database.py
+++ utilities/merge_database.py
@@ -11,6 +11,12 @@ strftime = datetime.strftime
 strptime = datetime.strptime
 
 
+def load_db(path):
+    with open(path) as f:
+        db = json.load(f)
+    return db
+
+
 def get_latest_time(gpu1, gpu2):
     """
     Return the most recent data collection time out of two GPU dictionaries.
@@ -34,72 +40,95 @@ def get_latest_time(gpu1, gpu2):
     return time2.strftime('%Y-%m-%d %H:%M:%S')
 
 
-data_directory = "U:/Work/6_Programming/Ebay_Scaper/New_data/"
+def make_new_entry(db_1, gpu2):
+    gpu1 = [g for g in db_1['collected']
+            if g['name'] == gpu2['name']][0]
+    new_entry = {'name': gpu1['name'],
+                 'data_collected': True,
+                 'collection_time': gpu1['collection_time'],
+                 'num_sold': 0,
+                 'data': gpu1['data']}
+    new_entry['collection_time'] = get_latest_time(gpu1, gpu2)
+    return new_entry
+
+
+def get_new_gpu_names(data_1, data_2):
+    # Add new GPU names
+    gpu_list1 = [gpu['name'] for gpu in data_1['collected']]
+    gpu_list2 = [gpu['name'] for gpu in data_2['collected']]
+    gpu_diff = [gpu for gpu in gpu_list2 if gpu not in gpu_list1]
+    gpu_shared = [gpu for gpu in gpu_list2 if gpu in gpu_list1]
+    return gpu_diff
+
+
+def already_in_db(item_new, new_entry):
+    for item_old in new_entry['data']:
+        if item_old == item_new:
+            return True
+    return False
+
+
+def get_db_index(db_new, name):
+    i = 0
+    for item in db_new['collected']:
+        if item['name'] == name:
+            return i
+        i += 1
+    return False
+
+
+data_directory = "C:/{your path here}"
 filepaths = os.listdir(data_directory)
 filepaths = [f for f in filepaths if '.json' in f]
 filepaths = [f for f in filepaths if 'combined' not in f]
 
 # Create new empty database
-new_db = {'collected': [],
+db_new = {'collected': [],
           'uncollected': []}
-new_db_name = 'combined_gpu_db.json'
-with open(data_directory + new_db_name, 'w', encoding='utf-8') as fout:
-    json.dump(new_db, fout, indent=4, sort_keys=False)
+db_new_name = 'combined_gpu_db.json'
+with open(data_directory + db_new_name, 'w', encoding='utf-8') as fout:
+    json.dump(db_new, fout, indent=4, sort_keys=False)
+
 
 # Iterate over all other json databases and combined into the
 # combined_gpu_db.json file
-for file1 in filepaths[:4]:
+for file1 in filepaths:
     print(file1)
     # Load existing combined database
-    with open(data_directory + new_db_name) as f:
-        db_1 = json.load(f)
 
-    # Load database to merge
-    with open(data_directory + file1) as f:
-        db_2 = json.load(f)
+    new_data = load_db(data_directory + file1)  # Load database to merge
+    gpu_diff = get_new_gpu_names(db_new, new_data)
 
-    # Add new GPU names
-    gpu_list1 = [gpu['name'] for gpu in db_1['collected']]
-    gpu_list2 = [gpu['name'] for gpu in db_2['collected']]
-    gpu_diff = [gpu for gpu in gpu_list2 if gpu not in gpu_list1]
-    gpu_shared = [gpu for gpu in gpu_list2 if gpu in gpu_list1]
+    for gpu in new_data['collected']:
+        if len(gpu["data"]) == 0:
+            continue
+        if gpu['name'] in gpu_diff:
+            print(f'New card added: {gpu["name"]}')
+            db_new['collected'].append(gpu)
+        else:
+            idx = get_db_index(db_new, gpu['name'])
 
-    new_db = {'collected': [],
-              'uncollected': []}
+            print(f'Merging data for: {gpu["name"]}')
+            # print(f'    {len(gpu["data"])} new cards')
+            # print(
+            # f'    {len(db_new["collected"][idx]["data"])} cards in database')
 
-    for gpu2 in db_2['collected']:
-        if gpu2['name'] in gpu_diff:
-            print(f'New card added: {gpu2["name"]}')
-            new_db['collected'].append(gpu2)
-        elif gpu2['name'] in gpu_shared:
-            print(f'Merging data for: {gpu2["name"]}')
-            gpu1 = [g for g in db_1['collected']
-                    if g['name'] == gpu2['name']][0]
-            new_entry = {'name': gpu1['name'],
-                         'data_collected': True,
-                         'collection_time': gpu1['collection_time'],
-                         'num_sold': 0,
-                         'data': gpu1['data']}
-
-            new_entry['collection_time'] = get_latest_time(gpu1, gpu2)
-            new_entry['num_sold'] += gpu2['num_sold']
-
+            new_entry = make_new_entry(db_new, gpu)
             items_removed = 0
-            for item in gpu2['data']:
-                in_new_data = False
-                for entry in new_entry['data']:
-                    shared_items = {
-                        k: entry[k] for k in entry if k in item and entry[k] == item[k]}
-                    if len(shared_items) == len(entry):
-                        in_new_data = True
-                if in_new_data:
+            for item_new in gpu['data']:
+                if already_in_db(item_new, new_entry):
                     items_removed += 1
-                else:
-                    new_entry['data'].append(item)
+                    continue
+
+                new_entry['data'].append(item_new)
             new_entry['num_sold'] = len(new_entry['data'])
-            new_db['collected'].append(new_entry)
-            print(f'    {items_removed} of {len(gpu2["data"])}'
-                  ' items shared')
+
+            db_new['collected'][idx] = new_entry
+
+            print(f'    {items_removed/(len(gpu["data"])):0.1%} '
+                  'of cards not added')
+    print(f'Number of GPUs in database {len(db_new["collected"])}')
     print('-' * 79)
-    with open(data_directory + new_db_name, 'w', encoding='utf-8') as fout:
-        json.dump(new_db, fout, indent=4, sort_keys=False)
+
+with open(data_directory + db_new_name, 'w', encoding='utf-8') as fout:
+    json.dump(db_new, fout, indent=4, sort_keys=False)
